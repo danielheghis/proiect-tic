c1a7458f197575b4ac60d6913a068f4a94a50e34
danielheghis <dani1220902018@gmail.com>
2025-01-26 16:57:25 +0200

    3. Firebase and firestore configuration

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const httpLogger = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv");
+dotenv.config();
+const {
+  uploadData,
+  initializeFirebaseApp,
+  getData,
+} = require("./firebase/firebase");
 
 const port = 3000;
 
@@ -10,7 +16,28 @@ app.use(httpLogger("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-dotenv.config();
+
+initializeFirebaseApp();
+
+app.get("/upload", async (req, res) => {
+  try {
+    await uploadData();
+    res.status(200).send("Data uploaded successfully");
+  } catch (error) {
+    console.error("Error uploading data", error.stack);
+    res.status(500).send("Error uploading data");
+  }
+});
+
+app.get("/get", async (req, res) => {
+  try {
+    const data = await getData();
+    res.status(200).send(data);
+  } catch (error) {
+    console.error("Error getting data", error.stack);
+    res.status(500).send("Error getting data");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
@@ -18,4 +45,5 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  console.log("Root endpoint accessed");
 });
---
+++ backend/firebase/firebase.js
@@ -0,0 +1,85 @@
+const { initializeApp } = require("firebase/app");
+const {
+  getFirestore,
+  doc,
+  setDoc,
+  getDocs,
+  query,
+  addDoc,
+  collection,
+} = require("firebase/firestore");
+
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+let app;
+let firestoreDb;
+
+const initializeFirebaseApp = () => {
+  try {
+    app = initializeApp(firebaseConfig);
+    firestoreDb = getFirestore();
+    console.log("Firestore initialized:");
+    return app;
+  } catch (error) {
+    console.error("Firebase initialization error", error.stack);
+  }
+};
+
+const uploadData = async () => {
+  const dataToUpload = {
+    key1: "ronaldo",
+    key2: "real madrid",
+  };
+  try {
+    const document = doc(firestoreDb, "receipts", "random-id");
+    let dataUpdated = await setDoc(document, dataToUpload);
+    return dataUpdated;
+  } catch (error) {
+    console.error("Error uploading data to firebase", error.stack);
+  }
+};
+
+const getData = async () => {
+  try {
+    const collectionRef = collection(firestoreDb, "receipts");
+    const finalData = [];
+    const q = query(collectionRef);
+
+    const docSnap = await getDocs(q);
+
+    docSnap.forEach((doc) => {
+      finalData.push(doc.data());
+    });
+
+    return finalData;
+  } catch (error) {
+    console.error("Error getting documents", error.stack);
+  }
+};
+
+const getFirebaseApp = () => {
+  return app;
+};
+
+module.exports = {
+  initializeFirebaseApp,
+  getFirebaseApp,
+  uploadData,
+  getData,
+};
